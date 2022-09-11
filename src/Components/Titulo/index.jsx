4cf9038e44dd657bbe5967fb375e206527f4bc4f
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-09-11 20:13:03 -0300

    Update index.jsx

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,11 @@
-import React from "react";
 
-const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-};
+import styled from "styled-components"
+
+
+const Titulo = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
+
 export default Titulo;
